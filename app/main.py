89b963a875b8ece00181948b7cc4a90a65e99518
Meet-02 <meetmourya04@gmail.com>
2026-02-14 22:35:31 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routes import auth
+from app.routes import auth,match
 
 app = FastAPI(title="Skillify API")
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(match.router, prefix="/match", tags=["Matching"])
